4a915ae83bc918ea47c28c9a3a7d49f8a473d70a
Jaroslav Miliunec <jaroslav.miliunec@abalt.lt>
2021-05-28 15:31:16 +0300

    init commit

---
+++ StartFmSoundcloudClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StartFmSoundcloudClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
